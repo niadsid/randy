c77c3aa09dd2fed5d4e2c13f124762a577a38998
niadsid <2010.david.westgate@gmail.com>
2011-07-05 10:43:06 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Randy::Application.config.secret_token = 'b88145b2e8323173c37e5c918fe90a094c2ca1624899631a51cdbba27ca25d9f4c52121e8d9f9e113ba103cacfe37625b907565dfd2893e984211ea4e716c03a'
